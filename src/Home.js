a9a5ebd9e1aff7ec605bfcce30f663c78e17f18d
Yatendrarajput <116754003+Yatendrarajput@users.noreply.github.com>
2023-06-29 01:50:19 +0530

    Update Home.js
    user image in footer removed

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchMovies } from "./MovieSlice";
 import ImgSlider from "./ImgSlider";
 import NewDisney from "./NewDisney";
@@ -8,12 +8,11 @@ import Originals from "./Originals";
 import Recommends from "./Recommends";
 import Trending from "./Trending";
 import Viwers from "./Viwers";
-import { selectUserName, selectUserPhoto } from "./userSlice";
+
 
 const Home = () => {
   const dispatch = useDispatch();
-  const userName = useSelector(selectUserName);
-  const userPhoto = useSelector(selectUserPhoto);
+
  
 
   useEffect(() => {
@@ -28,9 +27,7 @@ const Home = () => {
       <NewDisney />
       <Originals />
       <Trending />
-      <UserPhoto>
-        <img src={userPhoto} alt={userName} />
-      </UserPhoto>
+     
     </Container>
   );
 };
@@ -53,18 +50,6 @@ const Container = styled.main`
   }
 `;
 
-const UserPhoto = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-end;
-  padding: 0 20px;
 
-  img {
-    width: 40px;
-    height: 40px;
-    object-fit: cover;
-    border-radius: 50%;
-  }
-`;
 
 export default Home;
